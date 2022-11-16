ab12f5ae90aff6ba0fea1a8654ef9f5e3c3fca3d
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-16 17:22:54 +0700

    Membuat method untuk menge-check apakah node yang dimaksud ada di dalam list

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -61,6 +61,15 @@ namespace DoublyLinkedList
             newnode.next = current;
             newnode.prev = previous;
         }
+
+        /*Checks wheteher the specified node is present*/
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = START; current != null && rollNo != current.rollNumber; previous = current, current = current.next) 
+            { }
+            /*The above for loop traverses the list. If the specified node is found then the function returns true, otherwise false.*/
+            return (current != null);
+        }
     }
 
     class Program
