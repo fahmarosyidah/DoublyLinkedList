9a4dbb1dd2575f5d99ca70c24d59206f8519693c
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-16 16:59:23 +0700

    Deklarasi variabel

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -24,7 +24,15 @@ namespace DoublyLinkedList
         }
         public void addNode() /*Adds a new node*/
         {
-
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
         }
     }
 
