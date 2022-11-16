f40dcab0b83ff3be83741740e14ff4b019322d68
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-16 17:06:21 +0700

    Mengecek apakah daftarnya kosong

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -33,6 +33,30 @@ namespace DoublyLinkedList
             Node newnode = new Node();
             newnode.rollNumber = rollNo;
             newnode.name = nm;
+            /*Checks if the list is empty*/
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = START; current != null && rollNo >= current.rollNumber; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
         }
     }
 
